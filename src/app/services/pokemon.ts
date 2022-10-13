f199370014ebdaea686d18bad6415b8af5e1c6ec
gaanti <gaskevich06@gmail.com>
2022-10-14 00:37:36 +0300

    Clarify api endpoints

--- src/app/services/pokemon.ts
+++ src/app/services/pokemon.ts
@@ -2,24 +2,34 @@ import {api} from "../@api/api";
 import {Pokemon} from "../types/pokemon";
 import {PokemonTypes} from "../types/pokemonTypes";
 
+export interface getPokemonsRequest {
+    limit?: number
+}
+export interface getPokemonRequest {
+    id: number
+}
+export interface getPokemonTypesRequest {
+    limit?: number
+}
+
 export const pizzaApi = api.injectEndpoints({
     endpoints: (builder) => ({
-        getPokemons: builder.query<Pokemon[], number|void>({
+        getPokemons: builder.query<Pokemon[], getPokemonsRequest>({
             // It is possible to specify limit of items with ?limit={{value}}
-            query: (queryArg) => ({
+            query: (queryArg: getPokemonsRequest) => ({
                 url: `/pokemon${queryArg && `?limit=${queryArg}`}`,
                 method: 'GET',
             }),
         }),
-        getPokemonById: builder.query<Pokemon, number>({
-            query: (queryArg) => ({
+        getPokemonById: builder.query<Pokemon, getPokemonRequest>({
+            query: (queryArg: getPokemonRequest) => ({
                 url: `/pokemon/${queryArg}`,
                 method: 'GET',
             }),
         }),
-        getPokemonTypes: builder.query<PokemonTypes, number|void>({
+        getPokemonTypes: builder.query<PokemonTypes, getPokemonTypesRequest>({
             // It is possible to specify limit of items with ?limit={{value}}
-            query: (queryArg) => ({
+            query: (queryArg: getPokemonTypesRequest) => ({
                 url: `/type${queryArg && `?limit=${queryArg}`}`,
                 method: 'GET',
             }),
